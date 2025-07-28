84614761c97c86b0834dd9bafdbad2c26fa2bd88
DylanMcKenzie <Dylan20061@outlook.com>
2025-07-28 21:32:53 +0100

    Fix base path for custom domain deployment

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/verglas-public">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/verglas-public/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
